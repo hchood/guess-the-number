1fc784d43286f0510983ca0935f461f7f209824f
Helen Hood <helen.c.hood@gmail.com>
2014-11-24 15:05:27 -0500

    Add get_guess method

--- game.rb
+++ game.rb
@@ -1,5 +1,18 @@
 require 'pry'
 
+##############################
+#        METHODS
+##############################
+
+def get_guess
+  print "Guess a number between 1 and #{MAX_NUM}: "
+  gets.chomp.to_i
+end
+
+##############################
+#        PROGRAM
+##############################
+
 # Set secret number
 MAX_NUM = 1000
 SECRET_NUM = rand(MAX_NUM + 1)
@@ -8,33 +21,29 @@ guesses = []
 puts "Secret number is: #{SECRET_NUM}\n\n"
 
 # Get initial guess
-print "Guess a number between 1 and #{MAX_NUM}: "
-guess = gets.chomp.to_i
+guess = get_guess
 
 # re-prompt for guess until guess is within the right range
 until guess >= 1 && guess <= MAX_NUM
   puts "That's not a valid input."
-  print "Guess a number between 1 and #{MAX_NUM}: "
-  guess = gets.chomp.to_i
+  guess = get_guess
 end
 
 # re-prompt for guess until provide a guess that hasn't already been guessed
 while guesses.include?(guess)
   puts "You've already guessed that number!"
-  print "Guess a number between 1 and #{MAX_NUM}: "
-  guess = gets.chomp.to_i
+  guess = get_guess
 end
 
 # until out of guesses, check whether it's right or wrong
-while guesses.length < MAX_GUESSES - 1
+while guesses.length <= MAX_GUESSES
   if guess == SECRET_NUM
     puts "Congratulations! You win!"
     exit
   else
     guesses << guess
     puts "Wrong! You have #{MAX_GUESSES - guesses.length} more guesses."
-    print "Guess a number between 1 and #{MAX_NUM}: "
-    guess = gets.chomp.to_i
+    guess = get_guess
   end
 end
 
